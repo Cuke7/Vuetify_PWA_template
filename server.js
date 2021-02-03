dcc545d2f3b71d192830bcafc564e5904b099618
Cuke7 <cassany.louis@gmail.com>
2021-02-03 14:27:55 +0100

    Updated the port number for express

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express');
+var enforce = require('express-sslify');
 
 const app = express();
 
-app.use(express.static('public'));
-app.enable('trust proxy'); //https://stackoverflow.com/a/49176816
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.use(function (req, res, next) {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-    } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-});
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 80, () => {
     console.log("Listening to requests on" + process.env.PORT);
